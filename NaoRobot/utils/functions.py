08c5fe1f58d0f47f58eb7dbb93a7a7d8a8e616ea
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-01 16:11:25 +0700

    Update functions.py

--- NaoRobot/utils/functions.py
+++ NaoRobot/utils/functions.py
@@ -40,7 +40,7 @@ from NaoRobot.utils.http import get, post
 async def restart(m: Message):
     if m:
         await start_restart_stage(m.chat.id, m.message_id)
-    execvp(executable, [executable, "-m", "wbb"])
+    execvp(executable, [executable, "-m", "NaoRobot"])
 
 
 def generate_captcha():
