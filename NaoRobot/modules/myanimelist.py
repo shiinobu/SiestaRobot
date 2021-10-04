c7d15aa85949074ecbc95e959f469c6c05aa19fc
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-04 08:24:49 +0700

    Update myanimelist.py

--- NaoRobot/modules/myanimelist.py
+++ NaoRobot/modules/myanimelist.py
@@ -188,15 +188,6 @@ def manga(update: Update, context: CallbackContext):
         )
 
 
-__help__ = """
-Get information about anime, manga or characters with the help of this module! All data is fetched from [MyAnimeList](https://myanimelist.net).
-*Available commands:*
- - /manime <anime>: returns information about the anime.
- - /mcharacter <character>: returns information about the character.
- - /mmanga <manga>: returns information about the manga.
- - /mupcoming: returns a list of new anime in the upcoming seasons.
- """
-
 __mod_name__ = "MyAnimeList"
 
 
