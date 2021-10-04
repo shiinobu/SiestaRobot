4d2f05260c994853841dea14c0b97bc86e0d2d71
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-04 08:57:52 +0700

    Update telegraph.py

--- NaoRobot/modules/telegraph.py
+++ NaoRobot/modules/telegraph.py
@@ -95,9 +95,5 @@ file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
-__help__ = """
- - /tm : Get Telegraph Link Of Replied Media
- - /txt: Get Telegraph Link of Replied Text
-"""
 
 __mod_name__ = "Telegraph"
