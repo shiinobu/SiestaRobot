35ded968f6c3eff833dfd7caafa5dd3eebad6616
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-04 08:41:51 +0700

    Update admin.py

--- NaoRobot/modules/admin.py
+++ NaoRobot/modules/admin.py
@@ -568,6 +568,21 @@ __help__ = """
 • adding the bot to the desired channel (as an admin!)
 • sending `/setlog` in the channel
 • forwarding the `/setlog` to the group
+
+*Warning to punish*:
+ • /warns <userhandle>*:* get a user's number, and reason, of warns.
+ • /warnlist*:* list of all current warning filters
+*Admins only:*
+ • /warn <userhandle>*:* warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
+ • /dwarn <userhandle>*:* warn a user and delete the message. After 3 warns, the user will be banned from the group. Can also be used as a reply.
+ • /resetwarn <userhandle>*:* reset the warns for a user. Can also be used as a reply.
+ • /addwarn <keyword> <reply message>*:* set a warning filter on a certain keyword. If you want your keyword to \
+be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is an angry user`.
+ • /nowarn <keyword>*:* stop a warning filter
+ • /warnlimit <num>*:* set the warning limit
+ • /strongwarn <on/yes/off/no>*:* If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
+"""
+
 """
 
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist, run_async=True)
