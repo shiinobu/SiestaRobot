68827803e966dfee7cb592b8d2a2dd3ba3133322
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-05 01:14:32 +0700

    Update alive.py

--- NaoRobot/modules/alive.py
+++ NaoRobot/modules/alive.py
@@ -4,21 +4,19 @@ import os
 from telethon import events, Button
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from telegram import __version__ as telever
 from NaoRobot.events import register
 from NaoRobot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/03f16e043365fc82a85b5.jpg"
+PHOTO = "https://telegra.ph/file/a0a51e6a9e0beba56ae5c.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   ken = event.sender.first_name
-  TEXT = "🌸 **Holla I'm Nao Tomori!** \n\n"
-  TEXT += "🌸 **I'm Working Properly** \n\n"
-  TEXT += "🌸 **My Master : [Sena](https://t.me/xgothboi)** \n\n"
-  TEXT += f"🌸 **Library Version : {telever}**\n\n"
-  TEXT += f"🌸 **Telethon Version : {tlhver}** \n\n"
-  TEXT += f"🌸 **Pyrogram Version : {pyrover}** \n\n"
+  TEXT = f"🌻 **Holla {ken}, I'm Nao Tomori!** \n\n"
+  TEXT += "🌻 **I'm Working Properly** \n\n"
+  TEXT += "🌻 **My Master : [Sena](https://t.me/xgothboi)** \n\n"
+  TEXT += f"🌻 **Telethon Version : {tlhver}** \n\n"
+  TEXT += f"🌻 **Pyrogram Version : {pyrover}** \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/naoex_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/kenbotsupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
