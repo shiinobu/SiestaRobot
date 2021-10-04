ad7918474e94698f91e97584ce1db8fd7834b873
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-04 09:21:43 +0700

    Update misc.py

--- NaoRobot/modules/helper_funcs/misc.py
+++ NaoRobot/modules/helper_funcs/misc.py
@@ -66,12 +66,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🔙", callback_data="nao_back"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("◀️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("🔙 Back", callback_data="nao_back"),
+             EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="nao_back")]]
+        pairs += [[EqInlineKeyboardButton("🔙 Back", callback_data="nao_back")]]
 
     return pairs
 
