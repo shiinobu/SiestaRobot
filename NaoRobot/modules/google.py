bf76037243fadb6ca2b635d297afaf2d19912e5e
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-05 23:56:43 +0700

    Update google.py

--- NaoRobot/modules/google.py
+++ NaoRobot/modules/google.py
@@ -279,8 +279,8 @@ async def apk(e):
 __mod_name__ = "◎Search"
 
 __help__ = """
- ❍ /google <text>*:* Perform a google search
- ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+ ❍ /google <query>*:* Perform a google search
+ ❍ /image <query>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
  ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
  ❍ /reverse: Does a reverse image search of the media which it was replied to.
  ❍ /gps <location>*:* Get gps location.
