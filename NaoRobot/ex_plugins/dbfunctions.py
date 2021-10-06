585e67dbf240d8b6b9bc49a941ad4b5d479f20c5
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-06 23:06:33 +0700

    Update dbfunctions.py

--- NaoRobot/ex_plugins/dbfunctions.py
+++ NaoRobot/ex_plugins/dbfunctions.py
@@ -389,7 +389,7 @@ async def get_gbans_count() -> int:
 
 
 async def is_gbanned_user(user_id: int) -> bool:
-    user = await gbansdb.find_one({"user_id": user_id})
+    user = gbansdb.find_one({"user_id": user_id})
     if not user:
         return False
     return True
@@ -399,14 +399,14 @@ async def add_gban_user(user_id: int):
     is_gbanned = await is_gbanned_user(user_id)
     if is_gbanned:
         return
-    return await gbansdb.insert_one({"user_id": user_id})
+    return gbansdb.insert_one({"user_id": user_id})
 
 
 async def remove_gban_user(user_id: int):
     is_gbanned = await is_gbanned_user(user_id)
     if not is_gbanned:
         return
-    return await gbansdb.delete_one({"user_id": user_id})
+    return gbansdb.delete_one({"user_id": user_id})
 
 
 async def _get_lovers(chat_id: int):
