91424000fc9b3f788f75c47dbbcbb065809f4b86
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-07 18:55:16 +0700

    Update img_pdf.py

--- NaoRobot/modules/img_pdf.py
+++ NaoRobot/modules/img_pdf.py
@@ -57,7 +57,7 @@ async def convert(
         img.save(img_path, "JPEG", quality=100)
 
     pdf = BytesIO(img2pdf.convert(documents))
-    pdf.name = "wbb.pdf"
+    pdf.name = "NaoRobot.pdf"
 
     if len(main_message.command) >= 2:
         pdf.name = main_message.text.split(None, 1)[1]
