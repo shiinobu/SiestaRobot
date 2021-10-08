809ee1df6c05d4b705a40ffc3bffb9d1fa6c04a0
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-08 16:46:46 +0700

    Update userinfo.py

--- NaoRobot/modules/userinfo.py
+++ NaoRobot/modules/userinfo.py
@@ -577,7 +577,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Info"
+__mod_name__ = "Info/About"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
