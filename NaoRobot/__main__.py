6f6f8cb8e6a6d98e4f3bce65624d6f3e694bd5c7
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-09 19:23:45 +0700

    Update __main__.py

--- NaoRobot/__main__.py
+++ NaoRobot/__main__.py
@@ -817,7 +817,6 @@ def main():
     else:
         LOGGER.info("Using long polling.")
         updater.start_polling(
-            allowed_updates=Update.ALL_TYPES,
             timeout=15, 
             read_latency=4, 
             drop_pending_updates=True,
