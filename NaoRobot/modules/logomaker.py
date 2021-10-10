11f0334d0f7d0495637500915f70f6d90159f43c
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-11 03:03:31 +0700

    Update logomaker.py

--- NaoRobot/modules/logomaker.py
+++ NaoRobot/modules/logomaker.py
@@ -22,7 +22,7 @@ async def logo_gen(event):
     xx = await event.reply("`Creating your logo...`")
     name = event.pattern_match.group(1)
     if not name:
-        await xx.reply("`Provide some text to draw!`")
+        await xx.edit("`Provide some text to draw!`")
     bg_, font_ = "", ""
     if event.reply_to_msg_id:
         temp = await event.get_reply_message()
