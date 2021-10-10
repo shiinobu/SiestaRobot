1cc0d2390353d39cfbe88dd9fc620443caecb0f9
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-11 05:13:06 +0700

    Update google.py

--- NaoRobot/modules/google.py
+++ NaoRobot/modules/google.py
@@ -276,7 +276,7 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "◎Search"
+__mod_name__ = "Search"
 
 __help__ = """
  ❍ /google <query>*:* Perform a google search
