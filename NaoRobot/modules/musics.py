08efdae157941ba178bdbfecce1fd9ca8c58acbe
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-11 05:12:28 +0700

    Update musics.py

--- NaoRobot/modules/musics.py
+++ NaoRobot/modules/musics.py
@@ -1,8 +1,7 @@
-__mod_name__ = "◎Music"
+__mod_name__ = "Music"
 
 __help__ = """
 /video or /vsong <i>query</i>: download video from youtube
-/deezer <i>query</i>: download from deezer
 /music or /song <i>query</i>: download song from yt servers. (API BASED)
 /lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
 """
