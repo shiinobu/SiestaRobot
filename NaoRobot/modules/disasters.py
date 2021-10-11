04115ae1b5a3cd286c7583e28379754b2848a267
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-11 22:46:47 +0700

    Update disasters.py

--- NaoRobot/modules/disasters.py
+++ NaoRobot/modules/disasters.py
@@ -446,7 +446,83 @@ def removetiger(update: Update, context: CallbackContext) -> str:
 @whitelist_plus
 def whitelistlist(update: Update, context: CallbackContext):
 
+__help__ = f"""
+*⚠️ Notice:*
+Commands listed here only work for users with special access and are mainly used for troubleshooting, debugging purposes.
+Group admins/group owners do not need these commands.
 
+  *List all special users:*
+ • `/dragons`*:* Lists all Dragon disasters
+ • `/demons`*:* Lists all Demon disasters
+ • `/tigers`*:* Lists all Tigers disasters
+ • `/wolves`*:* Lists all Wolf disasters
+ • `/heroes`*:* Lists all Hero Association members
+ • `/adddragon`*:* Adds a user to Dragon
+ • `/adddemon`*:* Adds a user to Demon
+ • `/addtiger`*:* Adds a user to Tiger
+ • `/addwolf`*:* Adds a user to Wolf
+ • `Add dev doesnt exist, devs should know how to add themselves`
+
+ • *Broadcast: (Bot owner only)*
+ •  *Note:* This supports basic markdown
+ • `/broadcastall`*:* Broadcasts everywhere
+ • `/broadcastusers`*:* Broadcasts too all users
+ • `/broadcastgroups`*:* Broadcasts too all groups
+
+  *Groups Info:*
+ • `/groups`*:* List the groups with Name, ID, members count as a txt
+ • `/leave <ID>`*:* Leave the group, ID must have hyphen
+ • `/stats`*:* Shows overall bot stats
+ • `/getchats`*:* Gets a list of group names the user has been seen in. Bot owner only
+ • `/ginfo username/link/ID`*:* Pulls info panel for entire group
+
+  *Access control:*
+ • `/ignore`*:* Blacklists a user from
+ •  using the bot entirely
+ • `/lockdown <off/on>`*:* Toggles bot adding to groups
+ • `/notice`*:* Removes user from blacklist
+ •`/ignoredlist`*:* Lists ignored users
+
+  *Module loading:*
+ • `/listmodules`*:* Prints modules and their names
+ • `/unload <name>`*:* Unloads module dynamically
+ • `/load <name>`*:* Loads module
+
+  *Speedtest:*
+ • `/speedtest`*:* Runs a speedtest and gives you 2 options to choose from, text or image output
+
+  *Global Bans:*
+ • `/gban user reason`*:* Globally bans a user
+ • `/ungban user reason`*:* Unbans the user from the global bans list
+
+  *Module loading:*
+ • `/listmodules`*:* Lists names of all modules
+ • `/load modulename`*:* Loads the said module to
+    memory without restarting.
+ • `/unload modulename`*:* Loads the said module from
+    memory without restarting.memory without restarting the bot
+
+  *Windows self hosted only:*
+ • `/reboot`*:* Restarts the bots service
+ • `/gitpull`*:* Pulls the repo and then restarts the bots service
+
+  *Debugging and Shell:*
+ • `/debug <on/off>`*:* Logs commands to updates.txt
+ • `/logs`*:* Run this in support group to get logs in pm
+ • `/eval`*:* Self explanatory
+ • `/sh`*:* Runs shell command
+ • `/shell`*:* Runs shell command
+ • `/clearlocals`*:* As the name goes
+ • `/dbcleanup`*:* Removes deleted accs and groups from db
+ • `/py`*:* Runs python code
+
+ *Global Bans:*
+ • `/gban <id> <reason>`*:* Gbans the user, works by reply too
+ • `/ungban`*:* Ungbans the user, same usage as gban
+ • `/gbanlist`*:* Outputs a list of gbanned users
+
+Visit @{SUPPORT_CHAT} for more information.
+"""
 
 SUDO_HANDLER = CommandHandler(("addsudo", "adddragon"), addsudo, run_async=True)
 SUPPORT_HANDLER = CommandHandler(("addsupport", "adddemon"), addsupport, run_async=True)
