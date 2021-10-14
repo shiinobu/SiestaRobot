c8291c23e23c4fdc0cc001e8b54bad9ea17e25b6
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-14 20:06:54 +0700

    Update userinfo.py

--- NaoRobot/modules/userinfo.py
+++ NaoRobot/modules/userinfo.py
@@ -426,28 +426,28 @@ def stats(update, context):
     uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
     botuptime = get_readable_time((time.time() - StartTime))
     status = "*╒════「 System statistics 」*\n\n"
-    status += "*➢ System Start time:* " + str(uptime) + "\n"
+    status += "*ϟ System Start time:* " + str(uptime) + "\n\n"
     uname = platform.uname()
-    status += "*➢ System:* " + str(uname.system) + "\n"
-    status += "*➢ Node name:* " + escape_markdown(str(uname.node)) + "\n"
-    status += "*➢ Release:* " + escape_markdown(str(uname.release)) + "\n"
-    status += "*➢ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
+    status += "*⍟ System:* " + str(uname.system) + "\n"
+    status += "*⍟ Node name:* " + escape_markdown(str(uname.node)) + "\n"
+    status += "*⍟ Release:* " + escape_markdown(str(uname.release)) + "\n"
+    status += "*⍟ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
     mem = virtual_memory()
     cpu = cpu_percent()
     disk = disk_usage("/")
-    status += "*➢ CPU:* " + str(cpu) + " %\n"
-    status += "*➢ RAM:* " + str(mem[2]) + " %\n"
-    status += "*➢ Storage:* " + str(disk[3]) + " %\n\n"
-    status += "*➢ Python Version:* " + python_version() + "\n"
-    status += "*➢ python-Telegram-Bot:* " + str(ptbver) + "\n"
-    status += "*➢ Uptime:* " + str(botuptime) + "\n"
+    status += "*⍟ CPU:* " + str(cpu) + " %\n"
+    status += "*⍟ RAM:* " + str(mem[2]) + " %\n"
+    status += "*⍟ Storage:* " + str(disk[3]) + " %\n\n"
+    status += "*⍟ Python Version:* " + python_version() + "\n"
+    status += "*⍟ Python-Telegram-Bot:* " + str(ptbver) + "\n"
+    status += "*⍟ Uptime:* " + str(botuptime) + "\n"
     try:
         update.effective_message.reply_text(
             status
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
             + f"\n\n[Support](https://t.me/{SUPPORT_CHAT}) | [Updates](https://t.me/KennedyProject)\n\n"
-            + "╘═════「 by [Kennedy](https://github.com/KennedyProject) 」\n",
+            + "╘═════「 by [Kennedy](https://t.me/xgothboi) | [Github](https://github.com/KennedyProject) 」\n",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
         )
@@ -461,7 +461,7 @@ def stats(update, context):
                     )
                     + f"\n\n [Support](https://t.me/{SUPPORT_CHAT}) | [Updates](https://t.me/KennedyProject)\n\n"
                 )
-                + "╘══「 by [Kennedy](https://github.com/KennedyProject) 」\n"
+                + "╘══「 by [Kennedy](https://github.com/KennedyProject) | [Github](https://github.com/KennedyProject) 」\n"
             ),
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
