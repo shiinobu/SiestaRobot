7ed79a5d1d91b2e6da7b85821633492108ee7eb7
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-14 23:28:15 +0700

    Update admin.py

--- NaoRobot/modules/admin.py
+++ NaoRobot/modules/admin.py
@@ -1,6 +1,6 @@
 import html
 
-from telegram import ParseMode, Update
+from telegram import ParseMode, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
