61cb44c228da3182cd6b47c76581acdc4ff27667
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-14 16:10:38 +0700

    Update __main__.py

--- NaoRobot/__main__.py
+++ NaoRobot/__main__.py
@@ -391,14 +391,14 @@ def nao_about_callback(update, context):
     elif query.data == "nao_basichelp":
         query.message.edit_text(
             text="🌼 I'm *NaoRobot*, a powerful group management bot built to help you manage your group easily.",
-                 "\n• I can restrict users.",
-                 "\n• I can greet users with customizable welcome messages and even set a group's rules.",
-                 "\n• I have an advanced anti-flood system.",
-                 "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.",
-                 "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords.",
-                 "\n• I check for admins' permissions before executing any command and more stuffs",
-                 "\n\n_naorobot's licensed under the GNU General Public License v3.0_",
-                 "\n\n Clict on button bellow to get basic help for NaoRobot.",
+            "\n• I can restrict users.",
+            "\n• I can greet users with customizable welcome messages and even set a group's rules.",
+            "\n• I have an advanced anti-flood system.",
+            "\n• I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.",
+            "\n• I have a note keeping system, blacklists, and even predetermined replies on certain keywords.",
+            "\n• I check for admins' permissions before executing any command and more stuffs",
+            "\n\n_naorobot's licensed under the GNU General Public License v3.0_",
+            "\n\n Clict on button bellow to get basic help for NaoRobot.",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
@@ -419,7 +419,7 @@ def nao_about_callback(update, context):
                  ]
                 ]
             ),
-         )
+        )
     elif query.data == "nao_mback":
         query.message.edit_text(
                 PM_START_TEXT,
