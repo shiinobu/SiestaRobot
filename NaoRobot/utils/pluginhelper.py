02632624c5143d2dee777a1d312f7adf5ad96267
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-14 18:34:01 +0700

    Update pluginhelper.py

--- NaoRobot/utils/pluginhelper.py
+++ NaoRobot/utils/pluginhelper.py
@@ -36,6 +36,15 @@ def get_user(message: Message, text: str) -> [int, str, None]:
     return user_s, reason_
 
 
+async def is_admin(event, user):
+    try:
+        sed = await event.client.get_permissions(event.chat_id, user)
+        is_mod = bool(sed.is_admin)
+    except:
+        is_mod = False
+    return is_mod
+
+
 def get_readable_time(seconds: int) -> int:
     count = 0
     ping_time = ""
