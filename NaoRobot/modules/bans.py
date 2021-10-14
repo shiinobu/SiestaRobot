84de2b76174efb99dbe6623a1854fd00cfa4e4f7
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-14 20:10:54 +0700

    Update bans.py

--- NaoRobot/modules/bans.py
+++ NaoRobot/modules/bans.py
@@ -127,7 +127,7 @@ def ban(update: Update, context: CallbackContext) -> str:
             f"<code> </code><b>• User:</b> {mention_html(member.user.id, html.escape(member.user.first_name))}"
         )
         if reason:
-            reply += f"<code> </code><b>• Reason:</b> \n{html.escape(reason)}"
+            reply += f"<code> </code><b>• Reason:</b> {html.escape(reason)}"
 
         bot.sendMessage(
             chat.id,
