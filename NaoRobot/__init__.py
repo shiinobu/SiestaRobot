afb7dfa5e9ae65b201cf161520c7f51e6b59dc75
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-15 12:46:10 +0700

    Update __init__.py

--- NaoRobot/__init__.py
+++ NaoRobot/__init__.py
@@ -85,6 +85,7 @@ if ENV:
     API_HASH = os.environ.get("API_HASH", None)
     SESSION_STRING = os.environ.get("SESSION_STRING", None)
     DB_URI = os.environ.get("DATABASE_URL")
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     ARQ_API = os.environ.get("ARQ_API", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
@@ -178,6 +179,7 @@ else:
     USERBOT_PREFIX = Config.USERBOT_PREFIX
     PHONE_NUMBER = Config.PHONE_NUMBER
     WORKERS = Config.WORKERS
+    REM_BG_API_KEY = Config.REM_BG_API_KEY
     BAN_STICKER = Config.BAN_STICKER
     ALLOW_EXCL = Config.ALLOW_EXCL
     CASH_API_KEY = Config.CASH_API_KEY
