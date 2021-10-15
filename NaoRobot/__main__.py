2647447361b11bb60abef4fe32a77336c62be4ce
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-15 12:28:51 +0700

    Update __main__.py

--- NaoRobot/__main__.py
+++ NaoRobot/__main__.py
@@ -820,7 +820,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Yes I'm alive ✨")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Yes I'm alive!")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
