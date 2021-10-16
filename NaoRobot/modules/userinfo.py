774eced36b593fd5702962c9f4a62dd102070fc2
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-16 14:26:46 +0700

    Update userinfo.py

--- NaoRobot/modules/userinfo.py
+++ NaoRobot/modules/userinfo.py
@@ -429,15 +429,8 @@ def stats(update, context):
     status += "*ϟ System Start time:* " + str(uptime) + "\n\n"
     uname = platform.uname()
     status += "*⍟ System:* " + str(uname.system) + "\n"
-    status += "*⍟ Node name:* " + escape_markdown(str(uname.node)) + "\n"
     status += "*⍟ Release:* " + escape_markdown(str(uname.release)) + "\n"
     status += "*⍟ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
-    mem = virtual_memory()
-    cpu = cpu_percent()
-    disk = disk_usage("/")
-    status += "*⍟ CPU:* " + str(cpu) + " %\n"
-    status += "*⍟ RAM:* " + str(mem[2]) + " %\n"
-    status += "*⍟ Storage:* " + str(disk[3]) + " %\n\n"
     status += "*⍟ Python Version:* " + python_version() + "\n"
     status += "*⍟ Python-Telegram-Bot:* " + str(ptbver) + "\n"
     status += "*⍟ Uptime:* " + str(botuptime) + "\n"
@@ -446,8 +439,7 @@ def stats(update, context):
             status
             + "\n*Bot statistics*:\n"
             + "\n".join([mod.__stats__() for mod in STATS])
-            + f"\n\n[Support](https://t.me/{SUPPORT_CHAT}) | [Updates](https://t.me/KennedyProject)\n\n"
-            + "╘═════「 by [Kennedy](https://t.me/xgothboi) | [Github](https://github.com/KennedyProject) 」\n",
+            + "\n\n╘═══「 by [Kennedy](https://t.me/xgothboi) | [Github](https://github.com/KennedyProject) 」\n",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
         )
@@ -459,8 +451,6 @@ def stats(update, context):
                         "\n*Bot statistics*:\n"
                         + "\n".join(mod.__stats__() for mod in STATS)
                     )
-                    + f"\n\n [Support](https://t.me/{SUPPORT_CHAT}) | [Updates](https://t.me/KennedyProject)\n\n"
-                )
                 + "╘══「 by [Kennedy](https://github.com/KennedyProject) | [Github](https://github.com/KennedyProject) 」\n"
             ),
             parse_mode=ParseMode.MARKDOWN,
