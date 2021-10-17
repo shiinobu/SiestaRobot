8f4d2da0fa6712f9c0c4ca91e94c089cafe89feb
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-17 19:58:30 +0700

    Update admin.py

--- NaoRobot/modules/admin.py
+++ NaoRobot/modules/admin.py
@@ -639,7 +639,7 @@ def unpin(update: Update, context: CallbackContext):
 
     if prev_message and is_group:
         try:
-            bot.unpinChatMessage(
+            context.bot.unpinChatMessage(
                 chat.id, prev_message.message_id
             )
             msg.reply_text(
