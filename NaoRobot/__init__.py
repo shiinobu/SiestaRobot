860d02c37989675599dcc3d63c184e997e053068
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-17 12:13:57 +0700

    Update __init__.py

--- NaoRobot/__init__.py
+++ NaoRobot/__init__.py
@@ -235,13 +235,6 @@ except BaseException:
     print("Userbot Error ! Have you added a STRING_SESSION in deploying??")
     sys.exit(1)
 
-ubot2 = Client(SESSION_NAME, API_ID, API_HASH)
-try:
-    ubot2.start()
-except BaseException:
-    print("Userbot Error ! Have you added a SESSION_NAME in deploying??")
-    sys.exit(1)
-
 pbot = Client(
     ":memory:",
     api_id=API_ID,
