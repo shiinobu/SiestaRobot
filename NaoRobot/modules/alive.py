c3abab571c209109f61f7d459071d2e0136db618
Sena <87056837+KennedyProject@users.noreply.github.com>
2021-10-18 14:22:41 +0700

    Update alive.py

--- NaoRobot/modules/alive.py
+++ NaoRobot/modules/alive.py
@@ -5,6 +5,7 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
+from NaoRobot import USERBOT_USERNAME as ubu
 from NaoRobot.events import register
 from NaoRobot import telethn as tbot
 
@@ -16,7 +17,7 @@ async def awake(event):
   ken = event.sender.first_name
   TEXT = f"**Hi {ken}, I'm Emiko Robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += "⚪ **My Master : [No Name](https://t.me/excrybaby)** \n\n"
+  TEXT += "⚪ **My Master : [No Name](https://t.me/ubu)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}`\n\n"
   TEXT += f"⚪ **Python Version :** `3.9.7` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
