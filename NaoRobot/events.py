5e9ef3404975f474323fdf3d262cc5a01379f27a
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-22 03:36:48 +0700

    Update events.py

--- NaoRobot/events.py
+++ NaoRobot/events.py
@@ -1,6 +1,4 @@
-
 import inspect
-import glob
 import logging
 import sys
 import re
@@ -176,42 +174,3 @@ def NaoRobot(**args):
     if pattern:
         if not ignore_unsafe:
             args["pattern"] = args["pattern"].replace("^.", unsafe_pattern, 1)
-
-
-def load_module(shortname):
-    if shortname.startswith("__"):
-        pass
-    elif shortname.endswith("_"):
-        import importlib
-        import NaoRobot.events
-
-        path = Path(f"NaoRobot/modules/{shortname}.py")
-        name = "NaoRobot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
-        print("Successfully imported " + shortname)
-    else:
-        import importlib
-        import NaoRobot.events
-
-        path = Path(f"NaoRobot/modules/{shortname}.py")
-        name = "NaoRobot.modules.{}".format(shortname)
-        spec = importlib.util.spec_from_file_location(name, path)
-        mod = importlib.util.module_from_spec(spec)
-        mod.register = register
-        mod.NaoRobot = NaoRobot
-        mod.tbot = telethn
-        mod.logger = logging.getLogger(shortname)
-        spec.loader.exec_module(mod)
-        sys.modules["NaoRobot.modules." + shortname] = mod
-        print("Successfully imported " + shortname)
-
-
-path = "NaoRobot/modules/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
-        load_module(shortname.replace(".py", ""))
