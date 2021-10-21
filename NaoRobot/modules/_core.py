8e513fe4dc84c009baebfb8582224fa1bd19394f
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-22 00:04:40 +0700

    Update _core.py

--- NaoRobot/modules/_core.py
+++ NaoRobot/modules/_core.py
@@ -9,7 +9,7 @@ from NaoRobot import OWNER_ID, DEV_USERS
 from NaoRobot import TEMP_DOWNLOAD_DIRECTORY as path
 from NaoRobot import TEMP_DOWNLOAD_DIRECTORY
 from datetime import datetime
-water = 'https://telegra.ph/file/aada0bf6a2c5be7dcd103.jpg'
+water = './NaoRobot/resources/emiko.png'
 client = tbot
 
 @register(pattern=r"^/send ?(.*)")
@@ -34,3 +34,49 @@ async def Prof(event):
          )
     else:
         await event.reply("No File Found!")
+
+
+
+from NaoRobot.events import load_module
+import asyncio
+import os
+from datetime import datetime
+from pathlib import Path
+
+@register(pattern="^/install")
+async def install(event):
+    if event.fwd_from:
+        return
+    if event.sender_id == OWNER_ID or event.sender_id == DEV_USERS:
+        pass
+    else:
+        return
+    if event.reply_to_msg_id:
+        try:
+            downloaded_file_name = (
+                await event.client.download_media(  # pylint:disable=E0602
+                    await event.get_reply_message(),
+                    "NaoRobot/modules/",  # pylint:disable=E0602
+                )
+            )
+            if "(" not in downloaded_file_name:
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                load_module(shortname.replace(".py", ""))
+                await event.reply("Installed.... 👍\n `{}`".format(
+                        os.path.basename(downloaded_file_name)
+                    ),
+                )
+            else:
+                os.remove(downloaded_file_name)
+                k = await event.reply("**Error!**\n⚠️Cannot Install! \n📂 File not supported \n Or Pre Installed Maybe..😁",
+                )
+                await asyncio.sleep(2)
+                await k.delete()
+        except Exception as e:  # pylint:disable=C0103,W0703
+            j = await event.reply(str(e))
+            await asyncio.sleep(3)
+            await j.delete()
+            os.remove(downloaded_file_name)
+    await asyncio.sleep(3)
+    await event.delete()
