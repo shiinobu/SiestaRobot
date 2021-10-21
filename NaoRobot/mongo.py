d90df64075709140f4c6fbb3840e49fa87d99262
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-22 00:12:19 +0700

    Update mongo.py

--- NaoRobot/mongo.py
+++ NaoRobot/mongo.py
@@ -17,7 +17,7 @@ client = MongoClient()
 client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
 db = motor[MONGO_DB]
-db = client["NaoRobot"]
+db = client["naoxrobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
