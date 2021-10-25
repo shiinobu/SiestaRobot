cf0234398098a0f7e48ccfb864c4fe326447ad0d
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-25 16:43:36 +0700

    Update disable.py

--- NaoRobot/modules/disable.py
+++ NaoRobot/modules/disable.py
@@ -334,16 +334,6 @@ if is_module_loaded(FILENAME):
     dispatcher.add_handler(COMMANDS_HANDLER)
     dispatcher.add_handler(TOGGLE_HANDLER)
 
-    __help__ = """
-• `/cmds`*:* check the current status of disabled commands
-
-*Admins only:*
-• `/enable <cmd name>`*:* enable that command
-• `/disable <cmd name>`*:* disable that command
-• `/enablemodule <module name>`*:* enable all commands in that module
-• `/disablemodule <module name>`*:* disable all commands in that module
-• `/listcmds`*:* list all possible toggleable commands
-"""
     __mod_name__ = "Disabling"
 
 else:
