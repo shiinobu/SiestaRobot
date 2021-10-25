60994f5c7e50ecd485cc4317735957021936578b
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-25 16:49:36 +0700

    Delete musics.py

--- NaoRobot/modules/musics.py
+++
@@ -1,7 +0,0 @@
-__mod_name__ = "Music"
-
-__help__ = """
-/video or /vsong <i>query</i>: download video from youtube
-/music or /song <i>query</i>: download song from yt servers. (API BASED)
-/lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
-"""
