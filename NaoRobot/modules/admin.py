042a6b5e218b871e97615bd860b3d9d1060383f7
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-25 16:25:34 +0700

    Update admin.py

--- NaoRobot/modules/admin.py
+++ NaoRobot/modules/admin.py
@@ -880,6 +880,22 @@ __help__ = """
   • `/rules`*:* get the rules for this chat.
   • `/setrules <your rules here>`*:* set the rules for this chat.
   • `/clearrules`*:* clear the rules for this chat.
+*User Commands:*
+  • `/kickme`*:* kicks the user who issued the command
+  
+*Admins only:*
+  • `/ban <userhandle>`*:* bans a user. (via handle, or reply)
+  banme
+  • `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
+  • `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+  • `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
+  • `/kick <userhandle>`*:* kicks a user out of the group, (via handle, or reply)
+  • `/mute <userhandle>`*:* silences a user. Can also be used as a reply, muting the replied to user.
+  • `/tmute <userhandle> x(m/h/d)`*:* mutes a user for x time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
+  • `/unmute <userhandle>`*:* unmutes a user. Can also be used as a reply, muting the replied to user.
+  • `/zombies`*:* searches deleted accounts
+  • `/zombies clean`*:* removes deleted accounts from the group.
+  • `/snipe <chatid> <string>`*:* Make me send a message to a specific chat.
 """
 
 SET_DESC_HANDLER = CommandHandler("setdesc", set_desc, filters=Filters.chat_type.groups, run_async=True)
