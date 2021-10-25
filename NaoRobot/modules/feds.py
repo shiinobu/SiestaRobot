debd8a898b7334c5c2ce67f1f49ce7021319e6e8
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-25 16:45:59 +0700

    Update feds.py

--- NaoRobot/modules/feds.py
+++ NaoRobot/modules/feds.py
@@ -2405,19 +2405,6 @@ def fed_user_help(update: Update, context: CallbackContext):
 
 __mod_name__ = "Federations"
 
-__help__ = """
-Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
-But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
-*No longer!* With Federation, you can make a ban in one chat overlap with all other chats.\n
-You can even designate federation admins, so your trusted admin can ban all the spammers from chats you want to protect.\n
-
-*Commands:*\n
-Feds are now divided into 3 sections for your ease.
-• `/fedownerhelp`*:* Provides help for fed creation and owner only commands
-• `/fedadminhelp`*:* Provides help for fed administration commands
-• `/feduserhelp`*:* Provides help for commands anyone can use
-
-"""
 
 NEW_FED_HANDLER = CommandHandler("newfed", new_fed, run_async=True)
 DEL_FED_HANDLER = CommandHandler("delfed", del_fed, run_async=True)
