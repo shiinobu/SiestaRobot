4d89b9a208224775503f3dbc0ac13820c91e7169
KennedyProject <87056837+KennedyProject@users.noreply.github.com>
2021-10-26 08:08:07 +0700

    Update apakah.py

--- NaoRobot/modules/apakah.py
+++ NaoRobot/modules/apakah.py
@@ -1,6 +1,6 @@
 import html
 import random
-import NaoRobot.modules.apakah_string
+import NaoRobot.modules.apakah_string as apakontol_string
 from NaoRobot import dispatcher
 from telegram import ParseMode, Update, Bot
 from NaoRobot.modules.disable import DisableAbleCommandHandler
@@ -9,7 +9,7 @@ from telegram.ext import CallbackContext, run_async
 
 def apakah(update: Update, context: CallbackContext):
     args = context.args
-    update.effective_message.reply_text(random.choice(apakah_string.APAKAH))
+    update.effective_message.reply_text(random.choice(apakontol_string.APAKAH))
 
 
 APAKAH_HANDLER = DisableAbleCommandHandler("apakah", apakah, run_async=True)
