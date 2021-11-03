3ea0c9373a6f60e3da07af733ca8031f0f676b9c
Muhammad Sena Al-Kahfi <87056837+KennedyProject@users.noreply.github.com>
2021-11-03 23:15:43 +0700

    Update logomaker.py

--- NaoRobot/modules/logomaker.py
+++ NaoRobot/modules/logomaker.py
@@ -7,6 +7,41 @@ from NaoRobot.events import register
 from NaoRobot import telethn as tbot, ubot2 as ubot
 
 
+def mediainfo(media):
+    xx = str((str(media)).split("(", maxsplit=1)[0])
+    m = ""
+    if xx == "MessageMediaDocument":
+        mim = media.document.mime_type
+        if mim == "application/x-tgsticker":
+            m = "sticker animated"
+        elif "image" in mim:
+            if mim == "image/webp":
+                m = "sticker"
+            elif mim == "image/gif":
+                m = "gif as doc"
+            else:
+                m = "pic as doc"
+        elif "video" in mim:
+            if "DocumentAttributeAnimated" in str(media):
+                m = "gif"
+            elif "DocumentAttributeVideo" in str(media):
+                i = str(media.document.attributes[0])
+                if "supports_streaming=True" in i:
+                    m = "video"
+                m = "video as doc"
+            else:
+                m = "video"
+        elif "audio" in mim:
+            m = "audio"
+        else:
+            m = "document"
+    elif xx == "MessageMediaPhoto":
+        m = "pic"
+    elif xx == "MessageMediaWebPage":
+        m = "web"
+    return m
+
+
 @register(pattern="^/logo ?(.*)")
 async def logo_gen(event):
     xx = await event.reply("`Preparing your logo...`")
