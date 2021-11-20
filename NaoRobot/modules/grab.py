073d11e1904580b703c3e22d9661e949bf0f5aba
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-21 03:18:14 +0700

    Create grab.py

---
+++ NaoRobot/modules/grab.py
@@ -0,0 +1,23 @@
+from telethon.tl.functions.account import UpdateProfileRequest
+from telethon.tl.functions.photos import DeletePhotosRequest, UploadProfilePhotoRequest
+from NaoRobot import ubot2
+from NaoRobot.events import register
+
+
+@register(pattern="^/grab ?(.*)")
+async def gpoto(e):
+    kontol = e.pattern_match.group(0)
+    a = await e.reply("`Getting this person pfp`")
+    if ult:
+        pass
+    elif e.is_reply:
+        gs = await e.get_reply_message()
+        kontol = gs.sender_id
+    else:
+        kontol = e.chat_id
+    okla = await ubot2.download_profile_photo(kontol)
+    if not okla:
+        return await a.edit(a, "`Pfp Not Found...`")
+    await a.delete()
+    await e.reply(file=okla)
+    os.remove(okla)
