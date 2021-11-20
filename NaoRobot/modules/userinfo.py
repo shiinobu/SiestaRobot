543cdb36ab317b5a5f4b5a097f360f09f1ed42db
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-20 16:17:10 +0700

    Update userinfo.py

--- NaoRobot/modules/userinfo.py
+++ NaoRobot/modules/userinfo.py
@@ -450,7 +450,7 @@ def set_about_me(update: Update, context: CallbackContext):
 
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "卍 <b>Emiko Robot Statistics:</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "卍 Stats Of <b><a href='https://t.me/EmiexRobot'>Emiko Robot</a>:</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
     update.effective_message.reply_text(
         result,
