cb246cfccaec9d7cca745c89d868daedf148a83e
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-21 16:36:30 +0700

    Update warns.py

--- NaoRobot/modules/warns.py
+++ NaoRobot/modules/warns.py
@@ -493,6 +493,18 @@ def __chat_settings__(chat_id, user_id):
         f"It takes `{limit}` warns before the user gets *{'kicked' if soft_warn else 'banned'}*."
     )
 
+__help__ = """
+
+❍ /warns <userhandle>: get a user's number, and reason, of warns.
+❍ /warnlist: list of all current warning filters
+❍ /warn <userhandle>: warn a user. After 3 warns, the user will be banned from the group. Can also be used as a reply.
+❍ /dwarn <userhandle>: warn a user and delete the message. After 3 warns, the user will be banned from the group. Can also be used as a reply.
+❍ /resetwarn <userhandle>: reset the warns for a user. Can also be used as a reply.
+❍ /addwarn <keyword> <reply message>: set a warning filter on a certain keyword. If you want your keyword to be a sentence, encompass it with quotes, as such: /addwarn "very angry" This is an angry user.
+❍ /nowarn <keyword>: stop a warning filter
+❍ /warnlimit <num>: set the warning limit
+❍ /strongwarn <on/yes/off/no>: If set to on, exceeding the warn limit will result in a ban. Else, will just punch.
+"""
 
 __mod_name__ = "Warning"
 
