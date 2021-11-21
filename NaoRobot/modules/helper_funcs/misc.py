6c2a44139487d850e5e7964fc808b1f194f1a6ff
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-21 23:22:00 +0700

    Update misc.py

--- NaoRobot/modules/helper_funcs/misc.py
+++ NaoRobot/modules/helper_funcs/misc.py
@@ -72,7 +72,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs.append((modules[-1],))
 
     else:
-        pairs += [[EqInlineKeyboardButton("Go Back", callback_data="nao_back")]]
+        pairs += [[EqInlineKeyboardButton("Home", callback_data="nao_back")]]
 
     return pairs
 
