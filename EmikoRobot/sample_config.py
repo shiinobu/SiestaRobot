b89b59ccca2ed60e5a58b43d3978beb1572430f8
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 11:48:40 +0700

    Emiko Robot

--- EmikoRobot/sample_config.py
+++ EmikoRobot/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/NaoRobot/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/EmikoRobot/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
