9bfa504083d982dee2d6e4e289b1b57cad5eddf4
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:03:23 +0700

    y

--- EmikoRobot/services/tasks.py
+++ EmikoRobot/services/tasks.py
@@ -2,8 +2,8 @@ from asyncio import Lock, create_task
 from time import time
 from pyrogram import filters
 from pyrogram.types import Message
-from NaoRobot import BOT_ID, BOT_USERNAME, DEV_USERS
-from NaoRobot.services.sections import bold, section, w
+from EmikoRobot import BOT_ID, BOT_USERNAME, DEV_USERS
+from EmikoRobot.services.sections import bold, section, w
 tasks = {}
 TASKS_LOCK = Lock()
 arrow = lambda x: (x.text if x else "") + "\n`→`"
