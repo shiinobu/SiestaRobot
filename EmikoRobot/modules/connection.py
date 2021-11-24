f3c7d1308b770958e8ea0886f4370442ecb405b6
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 15:16:21 +0700

    Update and rename NaoRobot/modules/connection.py to EmikoRobot/modules/connection.py

--- EmikoRobot/modules/connection.py
+++ EmikoRobot/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler
 
-import NaoRobot.modules.sql.connection_sql as sql
-from NaoRobot import dispatcher, DRAGONS, DEV_USERS
-from NaoRobot.modules.helper_funcs import chat_status
-from NaoRobot.modules.helper_funcs.alternate import send_message, typing_action
+import EmikoRobot.modules.sql.connection_sql as sql
+from EmikoRobot import dispatcher, DRAGONS, DEV_USERS
+from EmikoRobot.modules.helper_funcs import chat_status
+from EmikoRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
@@ -406,13 +406,14 @@ __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
 This allows you to connect to a chat's database, and add things to it without the commands appearing in chat! For obvious reasons, you need to be an admin to add things; but any member in the group can view your data.
 
- • /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
- • /connection: List connected chats
- • /disconnect: Disconnect from a chat
- • /helpconnect: List available commands that can be used remotely
+❂ /connect: Connects to chat (Can be done in a group by /connect or /connect <chat id> in PM)
+❂ /connection: List connected chats
+❂ /disconnect: Disconnect from a chat
+❂ /helpconnect: List available commands that can be used remotely
 
 *Admin only:*
- • /allowconnect <yes/no>: allow a user to connect to a chat
+
+❂ /allowconnect <yes/no>: allow a user to connect to a chat
 """
 
 CONNECT_CHAT_HANDLER = CommandHandler(
