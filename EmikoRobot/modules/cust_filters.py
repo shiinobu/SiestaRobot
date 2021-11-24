8d180982a5758eeadb42510d5be6f76b2c62057a
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 15:22:08 +0700

    Update and rename NaoRobot/modules/cust_filters.py to EmikoRobot/modules/cust_filters.py

--- EmikoRobot/modules/cust_filters.py
+++ EmikoRobot/modules/cust_filters.py
@@ -13,23 +13,23 @@ from telegram.ext import (
     Filters,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from NaoRobot import dispatcher, LOGGER, DRAGONS
-from NaoRobot.modules.disable import DisableAbleCommandHandler
-from NaoRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from NaoRobot.modules.helper_funcs.chat_status import user_admin
-from NaoRobot.modules.helper_funcs.extraction import extract_text
-from NaoRobot.modules.helper_funcs.filters import CustomFilters
-from NaoRobot.modules.helper_funcs.misc import build_keyboard_parser
-from NaoRobot.modules.helper_funcs.msg_types import get_filter_type
-from NaoRobot.modules.helper_funcs.string_handling import (
+from EmikoRobot import dispatcher, LOGGER, DRAGONS
+from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from EmikoRobot.modules.helper_funcs.handlers import MessageHandlerChecker
+from EmikoRobot.modules.helper_funcs.chat_status import user_admin
+from EmikoRobot.modules.helper_funcs.extraction import extract_text
+from EmikoRobot.modules.helper_funcs.filters import CustomFilters
+from EmikoRobot.modules.helper_funcs.misc import build_keyboard_parser
+from EmikoRobot.modules.helper_funcs.msg_types import get_filter_type
+from EmikoRobot.modules.helper_funcs.string_handling import (
     split_quotes,
     button_markdown_parser,
     escape_invalid_curly_brackets,
     markdown_to_html,
 )
-from NaoRobot.modules.sql import cust_filters_sql as sql
-from NaoRobot.modules.connection import connected
-from NaoRobot.modules.helper_funcs.alternate import send_message, typing_action
+from EmikoRobot.modules.sql import cust_filters_sql as sql
+from EmikoRobot.modules.connection import connected
+from EmikoRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 HANDLER_GROUP = 10
 
@@ -607,9 +607,9 @@ def __chat_settings__(chat_id, user_id):
 
 
 __help__ = """
- • `/filters`*:* List all active filters saved in the chat.
+❂ /filters*:* List all active filters saved in the chat.
 *Admin only:*
- • `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
+❂ /filter <keyword> <reply message>*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
 is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
 keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
 doin?
@@ -621,11 +621,14 @@ doin?
  Reply 2
  %%%
  Reply 3`
- • `/stop <filter keyword>`*:* Stop that filter.
+
+❂ /stop <filter keyword>*:* Stop that filter.
+
 *Chat creator only:*
- • `/removeallfilters`*:* Remove all chat filters at once.
+
+❂ /removeallfilters*:* Remove all chat filters at once.
 *Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check `/markdownhelp` to know more!
+Check /markdownhelp to know more!
 """
 
 __mod_name__ = "Filters"
