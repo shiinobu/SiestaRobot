b70dd9d1cd76d55123f502dbb9dc2dc7a4c37a08
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:09:07 +0700

    Update and rename NaoRobot/utils/http.py to EmikoRobot/utils/http.py

--- EmikoRobot/utils/http.py
+++ EmikoRobot/utils/http.py
@@ -19,7 +19,7 @@ SOFTWARE.
 """
 from asyncio import gather
 
-from NaoRobot import aiohttpsession as session
+from EmikoRobot import aiohttpsession as session
 
 
 async def get(url: str, *args, **kwargs):
