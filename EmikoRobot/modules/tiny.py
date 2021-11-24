743f7e585ac1eed76a30ab77d182d1e8308f1922
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 17:04:00 +0700

    Update and rename NaoRobot/modules/tiny.py to EmikoRobot/modules/tiny.py

--- EmikoRobot/modules/tiny.py
+++ EmikoRobot/modules/tiny.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 from PIL import Image
-from NaoRobot.events import register
-from NaoRobot import telethn as tbot
+from EmikoRobot.events import register
+from EmikiRobot import telethn as tbot
 
 
 @register(pattern="^/tiny ?(.*)")
@@ -13,7 +13,7 @@ async def _(event):
            return
     kontol = await event.reply("`Processing tiny...`")
     ik = await tbot.download_media(reply)
-    im1 = Image.open("NaoRobot/resources/ken.png")
+    im1 = Image.open("EmikoRobot/resources/ken.png")
     if ik.endswith(".tgs"):
         await tbot.download_media(reply, "ken.tgs")
         os.system("lottie_convert.py ken.tgs json.json")
