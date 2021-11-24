f7c8849510e754766fa7cdd3e29dcaece9537475
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:41:38 +0700

    Update and rename NaoRobot/modules/sql/cust_filters_sql.py to EmikoRobot/modules/sql/cust_filters_sql.py

--- EmikoRobot/modules/sql/cust_filters_sql.py
+++ EmikoRobot/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from NaoRobot.modules.helper_funcs.msg_types import Types
-from NaoRobot.modules.sql import BASE, SESSION
+from EmikoRobot.modules.helper_funcs.msg_types import Types
+from EmikoRobot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
