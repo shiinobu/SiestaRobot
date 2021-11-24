5ed039b4e2e4e209eb2188532fc4d5a1acdcac2f
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:48:51 +0700

    Update and rename NaoRobot/modules/sql/notes_sql.py to EmikoRobot/modules/sql/notes_sql.py

--- EmikoRobot/modules/sql/notes_sql.py
+++ EmikoRobot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from NaoRobot.modules.helper_funcs.msg_types import Types
-from NaoRobot.modules.sql import BASE, SESSION
+from EmikoRobot.modules.helper_funcs.msg_types import Types
+from EmikoRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String, UnicodeText, distinct, func
 
 
