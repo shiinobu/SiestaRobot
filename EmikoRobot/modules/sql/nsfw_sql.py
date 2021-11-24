45b36275960860e113805296f9253b9f87648806
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:49:32 +0700

    Update and rename NaoRobot/modules/sql/nsfw_sql.py to EmikoRobot/modules/sql/nsfw_sql.py

--- EmikoRobot/modules/sql/nsfw_sql.py
+++ EmikoRobot/modules/sql/nsfw_sql.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String
-from NaoRobot.modules.sql import BASE, SESSION
+from EmikoRobot.modules.sql import BASE, SESSION
 #   |----------------------------------|
 #   |  Test Module by @EverythingSuckz |
 #   |        Kang with Credits         |
