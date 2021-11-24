e9e6b90515af6ff2f3b11e0d81f9f33151924d68
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:33:31 +0700

    Update and rename NaoRobot/modules/sql/__init__.py to EmikoRobot/modules/sql/__init__.py

--- EmikoRobot/modules/sql/__init__.py
+++ EmikoRobot/modules/sql/__init__.py
@@ -1,4 +1,4 @@
-from NaoRobot import DB_URI
+from EmikoRobot import DB_URI
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
