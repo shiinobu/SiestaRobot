7a8ddc119cefce40501b1800a47d9559f37a8fe4
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 14:33:09 +0700

    Update and rename NaoRobot/modules/alive.py to EmikoRobot/modules/alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -5,9 +5,8 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from NaoRobot import USERBOT_USERNAME as ubu
-from NaoRobot.events import register
-from NaoRobot import telethn as tbot
+from EmikoRobot.events import register
+from EmikoRobot import telethn as tbot
 
 
 PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
@@ -16,7 +15,8 @@ PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
 async def awake(event):
   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
   TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [sena-ex](https://t.me/{ubu})** \n\n"
+  TEXT += f"⚪ **My Master : [sena-ex](https://t.me/excrybaby)** \n\n"
+  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
   TEXT += "**Thanks For Adding Me Here ❤️**"
