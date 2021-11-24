cd554784eb3417a4b8a6fc6d6c55988da2fedbb9
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:23:04 +0700

    Update and rename NaoRobot/modules/modules.py to EmikoRobot/modules/modules.py

--- EmikoRobot/modules/modules.py
+++ EmikoRobot/modules/modules.py
@@ -1,8 +1,8 @@
 import importlib
 import collections
 
-from NaoRobot import dispatcher, telethn
-from NaoRobot.__main__ import (
+from EmikoRobot import dispatcher, telethn
+from EmikoRobot.__main__ import (
     CHAT_SETTINGS,
     DATA_EXPORT,
     DATA_IMPORT,
@@ -13,7 +13,7 @@ from NaoRobot.__main__ import (
     USER_INFO,
     USER_SETTINGS,
 )
-from NaoRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from EmikoRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 
