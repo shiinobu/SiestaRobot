7ab1b3230b1f3828170938f395ce71729221a000
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:04:16 +0700

    y

--- EmikoRobot/services/telethonbasics.py
+++ EmikoRobot/services/telethonbasics.py
@@ -2,7 +2,7 @@ import os
 
 from telethon.tl.types import *
 
-from NaoRobot.utils.pluginhelper import runcmd
+from EmikoRobot.utils.pluginhelper import runcmd
 
 
 async def convert_to_image(event, borg):
