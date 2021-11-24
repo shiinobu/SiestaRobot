3a7e7b7b19cdea0b8a423f6b889c7126227ddfa1
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 17:10:07 +0700

    Update and rename NaoRobot/modules/urluploader.py to EmikoRobot/modules/urluploader.py

--- EmikoRobot/modules/urluploader.py
+++ EmikoRobot/modules/urluploader.py
@@ -3,7 +3,7 @@ import time
 
 import aiohttp
 
-from NaoRobot.utils.uputils import humanbytes, time_formatter
+from EmikoRobot.utils.uputils import humanbytes, time_formatter
 
 
 async def download_file(url, file_name, message, start_time, bot):
