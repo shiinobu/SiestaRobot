6e7e20b0ec34f65ae6cb3ff024427f16e64f540f
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:45:27 +0700

    Update and rename NaoRobot/modules/remote_cmds.py to EmikoRobot/modules/remote_cmds.py

--- EmikoRobot/modules/remote_cmds.py
+++ EmikoRobot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from NaoRobot import dispatcher, LOGGER
-from NaoRobot.modules.helper_funcs.chat_status import (
+from EmikoRobot import dispatcher, LOGGER
+from EmikoRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from NaoRobot.modules.helper_funcs.extraction import extract_user_and_text
-from NaoRobot.modules.helper_funcs.filters import CustomFilters
+from EmikoRobot.modules.helper_funcs.extraction import extract_user_and_text
+from EmikoRobot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
