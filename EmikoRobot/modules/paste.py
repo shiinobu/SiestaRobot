fc5da92148be8cefc180fb508f88825f1516bfff
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:35:40 +0700

    Update and rename NaoRobot/modules/paste.py to EmikoRobot/modules/paste.py

--- EmikoRobot/modules/paste.py
+++ EmikoRobot/modules/paste.py
@@ -1,22 +1,3 @@
-"""
-MIT License
-Copyright (c) 2021 TheHamkerCat
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 import asyncio
 import os
 import re
@@ -26,10 +7,10 @@ from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
 
-from NaoRobot import aiohttpsession as session
-from NaoRobot import pbot as app
-from NaoRobot.utils.errors import capture_err
-from NaoRobot.utils.pastebin import paste
+from EmikoRobot import aiohttpsession as session
+from EmikoRobot import pbot as app
+from EmikoRobot.utils.errors import capture_err
+from EmikoRobot.utils.pastebin import paste
 
 __mod_name__ = "Paste​"
 
