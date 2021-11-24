45912c9b4453199210a5c46b4a590029a4864544
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-25 01:11:19 +0700

    Update tools.py

--- EmikoRobot/modules/tools.py
+++ EmikoRobot/modules/tools.py
@@ -29,8 +29,8 @@ Link To File:
 ❂ /transfersh: reply to a telegram file to upload it on transfersh and get direct download link
 ❂ /tmpninja: reply to a telegram file to upload it on tmpninja and get direct download link
  File Or Text To Telegraph:
-❂ /tm : Get Telegraph Link Of Replied Media
-❂ /txt: Get Telegraph Link of Replied Text
+❂ /tgm : Get Telegraph Link Of Replied Media
+❂ /tgt: Get Telegraph Link of Replied Text
 
  Tagger
 ❂ /tagall : for tag everyone at least 100 member first.
