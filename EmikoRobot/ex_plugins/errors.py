6ea49f22bcdc1ec5d75b096a9f4f9c0b864d796b
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 11:56:06 +0700

    Update and rename NaoRobot/ex_plugins/errors.py to EmikoRobot/ex_plugins/errors.py

--- EmikoRobot/ex_plugins/errors.py
+++ EmikoRobot/ex_plugins/errors.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from NaoRobot import EVENT_LOGS, pbot as app
+from EmikoRobot import EVENT_LOGS, pbot as app
 
 
 def split_limits(text):
