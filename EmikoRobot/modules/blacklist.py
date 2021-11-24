622ad583a46972ed6e9fd89dae6a2137ca3ea657
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 15:06:06 +0700

    Update and rename NaoRobot/modules/blacklist.py to EmikoRobot/modules/blacklist.py

--- EmikoRobot/modules/blacklist.py
+++ EmikoRobot/modules/blacklist.py
@@ -6,18 +6,18 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, MessageHandler, Filters
 from telegram.utils.helpers import mention_html
 
-import NaoRobot.modules.sql.blacklist_sql as sql
-from NaoRobot import dispatcher, LOGGER
-from NaoRobot.modules.disable import DisableAbleCommandHandler
-from NaoRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
-from NaoRobot.modules.helper_funcs.extraction import extract_text
-from NaoRobot.modules.helper_funcs.misc import split_message
-from NaoRobot.modules.log_channel import loggable
-from NaoRobot.modules.warns import warn
-from NaoRobot.modules.helper_funcs.string_handling import extract_time
-from NaoRobot.modules.connection import connected
-from NaoRobot.modules.sql.approve_sql import is_approved
-from NaoRobot.modules.helper_funcs.alternate import send_message, typing_action
+import EmikoRobot.modules.sql.blacklist_sql as sql
+from EmikoRobot import dispatcher, LOGGER
+from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from EmikoRobot.modules.helper_funcs.chat_status import user_admin, user_not_admin
+from EmikoRobot.modules.helper_funcs.extraction import extract_text
+from EmikoRobot.modules.helper_funcs.misc import split_message
+from EmikoRobot.modules.log_channel import loggable
+from EmikoRobot.modules.warns import warn
+from EmikoRobot.modules.helper_funcs.string_handling import extract_time
+from EmikoRobot.modules.connection import connected
+from EmikoRobot.modules.sql.approve_sql import is_approved
+from EmikoRobot.modules.helper_funcs.alternate import send_message, typing_action
 
 BLACKLIST_GROUP = 11
 
@@ -458,23 +458,23 @@ Blacklists are used to stop certain triggers from being said in a group. Any tim
 
 *NOTE*: Blacklists do not affect group admins.
 
- • `/blacklist`*:* View the current blacklisted words.
+❂ /blacklist*:* View the current blacklisted words.
 
 Admin only:
- • `/addblacklist <triggers>`*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
- • `/unblacklist <triggers>`*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
- • `/blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>`*:* Action to perform when someone sends blacklisted words.
+❂ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+❂ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
+❂ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
 
 Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, the message will be deleted immediately.
 *NOTE:* Blacklist stickers do not affect the group admin
- • `/blsticker`*:* See current blacklisted sticker
+❂ • /blsticker*:* See current blacklisted sticker
 *Only admin:*
- • `/addblsticker <sticker link>`*:* Add the sticker trigger to the black list. Can be added via reply sticker
- • `/unblsticker <sticker link>`*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
- • `/rmblsticker <sticker link>`*:* Same as above
- • `/blstickermode <delete/ban/tban/mute/tmute>`*:* sets up a default action on what to do if users use blacklisted stickers
+❂ /addblsticker <sticker link>*:* Add the sticker trigger to the black list. Can be added via reply sticker
+❂ /unblsticker <sticker link>*:* Remove triggers from blacklist. The same newline logic applies here, so you can delete multiple triggers at once
+❂ /rmblsticker <sticker link>*:* Same as above
+❂ /blstickermode <delete/ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
 Note:
- • `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
+❂ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
 
 """
 BLACKLIST_HANDLER = DisableAbleCommandHandler(
