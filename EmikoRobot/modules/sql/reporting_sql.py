b332b881fdccd2e21b8ab83e3ec3f4ab5550277b
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:51:35 +0700

    Update and rename NaoRobot/modules/sql/reporting_sql.py to EmikoRobot/modules/sql/reporting_sql.py

--- EmikoRobot/modules/sql/reporting_sql.py
+++ EmikoRobot/modules/sql/reporting_sql.py
@@ -1,7 +1,7 @@
 import threading
 from typing import Union
 
-from NaoRobot.modules.sql import BASE, SESSION
+from EmikoRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, String
 
 
