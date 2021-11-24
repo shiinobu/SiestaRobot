331c82eb18a67f1b724723f1c07cd99dab341017
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:36:55 +0700

    Update and rename NaoRobot/modules/sql/approve_sql.py to EmikoRobot/modules/sql/approve_sql.py

--- EmikoRobot/modules/sql/approve_sql.py
+++ EmikoRobot/modules/sql/approve_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Integer, func, distinct
 
-from NaoRobot.modules.sql import BASE, SESSION
+from EmikoRobot.modules.sql import BASE, SESSION
 
 
 class Approvals(BASE):
