e1a21d35bdc2004ea65f6de4883e5785373082b3
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-25 00:52:04 +0700

    Update telegraph.py

--- EmikoRobot/modules/telegraph.py
+++ EmikoRobot/modules/telegraph.py
@@ -17,7 +17,7 @@ r = telegraph.create_account(short_name=wibu)
 auth_url = r["auth_url"]
 
 
-@register(pattern="^/t(m|xt) ?(.*)")
+@register(pattern="^/t(gm|gt) ?(.*)")
 async def _(event):
     if event.fwd_from:
         return
@@ -26,15 +26,14 @@ async def _(event):
         start = datetime.now()
         r_message = await event.get_reply_message()
         input_str = event.pattern_match.group(1)
-        if input_str == "m":
-            downloaded_file_name = await tbot.download_media(
-                r_message, TMP_DOWNLOAD_DIRECTORY
+        if input_str == "gm":
+            downloaded_file_name = await telethn.download_media(
+                r_message,
+                TMP_DOWNLOAD_DIRECTORY
             )
             end = datetime.now()
             ms = (end - start).seconds
-            h = await event.reply(
-                "Downloaded to {} in {} seconds.".format(downloaded_file_name, ms)
-            )
+            h = await event.reply("Downloaded to {} in {} seconds.".format(downloaded_file_name, ms))
             if downloaded_file_name.endswith((".webp")):
                 resize_image(downloaded_file_name)
             try:
@@ -47,13 +46,10 @@ async def _(event):
                 end = datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await h.edit(
-                    "Here is [Your telegra.ph](https://telegra.ph{}).",
-                    link_preview=True
-                )
-        elif input_str == "xt":
-            user_object = await tbot.get_entity(r_message.sender_id)
-            title_of_page = user_object.first_name  # + " " + user_object.last_name
+                await h.edit("Uploaded to https://telegra.ph{}".format(media_urls[0], link_preview=True)
+        elif input_str == "gt":
+            user_object = await telethn.get_entity(r_message.sender_id)
+            title_of_page = user_object.first_name # + " " + user_object.last_name
             # apparently, all Users do not have last_name field
             if optional_title:
                 title_of_page = optional_title
@@ -61,8 +57,9 @@ async def _(event):
             if r_message.media:
                 if page_content != "":
                     title_of_page = page_content
-                downloaded_file_name = await tbot.download_media(
-                    r_message, TMP_DOWNLOAD_DIRECTORY
+                downloaded_file_name = await telethn.download_media(
+                    r_message,
+                    TMP_DOWNLOAD_DIRECTORY
                 )
                 m_list = None
                 with open(downloaded_file_name, "rb") as fd:
@@ -71,15 +68,13 @@ async def _(event):
                     page_content += m.decode("UTF-8") + "\n"
                 os.remove(downloaded_file_name)
             page_content = page_content.replace("\n", "<br>")
-            response = telegraph.create_page(title_of_page, html_content=page_content)
+            response = telegraph.create_page(
+                title_of_page,
+                html_content=page_content
+            )
             end = datetime.now()
             ms = (end - start).seconds
-            await event.reply(
-                "Pasted to https://telegra.ph/{} in {} seconds.".format(
-                    response["path"], ms
-                ),
-                link_preview=True,
-            )
+            await event.reply("Pasted to https://telegra.ph/{}".format(response["path"]), link_preview=True)
     else:
         await event.reply("Reply to a message to get a permanent telegra.ph link.")
 
@@ -88,10 +83,8 @@ def resize_image(image):
     im = Image.open(image)
     im.save(image, "PNG")
 
-
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
-
 
 __mod_name__ = "Telegraph"
