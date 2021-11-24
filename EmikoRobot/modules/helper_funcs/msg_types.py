f85e2ab6b9722f51ee9e8f1416799adb5a031a91
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:29:18 +0700

    Update and rename NaoRobot/modules/helper_funcs/msg_types.py to EmikoRobot/modules/helper_funcs/msg_types.py

--- EmikoRobot/modules/helper_funcs/msg_types.py
+++ EmikoRobot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from NaoRobot.modules.helper_funcs.string_handling import button_markdown_parser
+from EmikoRobot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
