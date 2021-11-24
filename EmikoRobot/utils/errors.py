72b77d2e5c7bb2aa89ef18f44a4c43e6c473f8bd
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:06:42 +0700

    y

--- EmikoRobot/utils/errors.py
+++ EmikoRobot/utils/errors.py
@@ -7,7 +7,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from NaoRobot import LOGGER, pbot as app
+from EmikoRobot import LOGGER, pbot as app
 
 
 def split_limits(text):
