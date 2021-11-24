5e3d9b287d1efd87450944137aa0e38777fb708a
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 15:08:38 +0700

    Update and rename NaoRobot/modules/blacklistusers.py to EmikoRobot/modules/blacklistusers.py

--- EmikoRobot/modules/blacklistusers.py
+++ EmikoRobot/modules/blacklistusers.py
@@ -1,7 +1,7 @@
 # Module to blacklist users and prevent them from using commands by @TheRealPhoenix
 import html
-import NaoRobot.modules.sql.blacklistusers_sql as sql
-from NaoRobot import (
+import EmikoRobot.modules.sql.blacklistusers_sql as sql
+from EmikoRobot import (
     DEV_USERS,
     OWNER_ID,
     DRAGONS,
@@ -10,12 +10,12 @@ from NaoRobot import (
     WOLVES,
     dispatcher,
 ) 
-from NaoRobot.modules.helper_funcs.chat_status import dev_plus
-from NaoRobot.modules.helper_funcs.extraction import (
+from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
+from EmikoRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from NaoRobot.modules.log_channel import gloggable
+from EmikoRobot.modules.log_channel import gloggable
 from telegram import ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler
