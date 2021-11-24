9cddade1f1041531021dd01bcbe187a2f10e119b
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 15:26:35 +0700

    Update and rename NaoRobot/modules/disable.py to EmikoRobot/modules/disable.py

--- EmikoRobot/modules/disable.py
+++ EmikoRobot/modules/disable.py
@@ -2,9 +2,9 @@ import importlib
 from typing import Union
 
 from future.utils import string_types
-from NaoRobot import dispatcher
-from NaoRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
-from NaoRobot.modules.helper_funcs.misc import is_module_loaded
+from EmikoRobot import dispatcher
+from EmikoRobot.modules.helper_funcs.handlers import CMD_STARTERS, SpamChecker
+from EmikoRobot.modules.helper_funcs.misc import is_module_loaded
 from telegram import ParseMode, Update
 from telegram.ext import (
     CallbackContext,
@@ -22,12 +22,12 @@ FILENAME = __name__.rsplit(".", 1)[-1]
 # If module is due to be loaded, then setup all the magical handlers
 if is_module_loaded(FILENAME):
 
-    from NaoRobot.modules.helper_funcs.chat_status import (
+    from EmikoRobot.modules.helper_funcs.chat_status import (
         connection_status,
         is_user_admin,
         user_admin,
     )
-    from NaoRobot.modules.sql import disable_sql as sql
+    from EmikoRobot.modules.sql import disable_sql as sql
 
     DISABLE_CMDS = []
     DISABLE_OTHER = []
@@ -318,14 +318,15 @@ if is_module_loaded(FILENAME):
 
 
     __help__ = """
-• /cmds*:* check the current status of disabled commands
+❂ /cmds*:* check the current status of disabled commands
 
 *Admins only:*
-• /enable <cmd name>*:* enable that command
-• /disable <cmd name>*:* disable that command
-• /enablemodule <module name>*:* enable all commands in that module
-• /disablemodule <module name>*:* disable all commands in that module
-• /listcmds*:* list all possible toggleable commands
+
+❂ /enable <cmd name>*:* enable that command
+❂ /disable <cmd name>*:* disable that command
+❂ /enablemodule <module name>*:* enable all commands in that module
+❂ /disablemodule <module name>*:* disable all commands in that module
+❂ /listcmds*:* list all possible toggleable commands
 """
 
     DISABLE_HANDLER = CommandHandler("disable", disable, run_async=True)
