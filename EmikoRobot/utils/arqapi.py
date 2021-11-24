81c94fb09f51c6cb8d6da0917295b2edd93e83af
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:06:01 +0700

    y

--- EmikoRobot/utils/arqapi.py
+++ EmikoRobot/utils/arqapi.py
@@ -1,19 +1,18 @@
-
 import json
 import sys
 from random import randint
 from time import time
 
 import aiohttp
-from LaylaRobot import aiohttpsession 
+from EmikoRobot import aiohttpsession 
 from aiohttp import ClientSession
 
 from google_trans_new import google_translator
 from Python_ARQ import ARQ
 from search_engine_parser import GoogleSearch
 
-from NaoRobot import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
-from NaoRobot import pbot
+from EmikoRobot import BOT_ID, OWNER_ID, ARQ_API_URL, ARQ_API_KEY
+from EmikoRobot import pbot
 
 ARQ_API = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
 ARQ_API_KEY = "WZQUBA-PFAZQJ-OMIINH-MIVHYM-ARQ"
