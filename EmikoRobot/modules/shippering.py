4706e879a0d5e43810ad4684f8d417e02a1c15c0
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:52:49 +0700

    Update and rename NaoRobot/modules/shippering.py to EmikoRobot/modules/shippering.py

--- EmikoRobot/modules/shippering.py
+++ EmikoRobot/modules/shippering.py
@@ -1,6 +1,6 @@
-from NaoRobot import pbot as app
-from NaoRobot.utils.errors import capture_err
-from NaoRobot.ex_plugins.dbfunctions import get_couple, save_couple
+from EmikoRobot import pbot as app
+from EmikoRobot.utils.errors import capture_err
+from EmikoRobot.ex_plugins.dbfunctions import get_couple, save_couple
 from pyrogram import filters
 import random
 from datetime import datetime
