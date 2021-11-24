29837006874535edf482da59e317e8a054c00be1
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:25:22 +0700

    Update and rename NaoRobot/modules/muting.py to EmikoRobot/modules/muting.py

--- EmikoRobot/modules/muting.py
+++ EmikoRobot/modules/muting.py
@@ -2,8 +2,8 @@ import html
 import re
 from typing import Optional
 
-from NaoRobot import LOGGER, TIGERS, dispatcher
-from NaoRobot.modules.helper_funcs.chat_status import (
+from EmikoRobot import LOGGER, TIGERS, dispatcher
+from EmikoRobot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     connection_status,
@@ -11,12 +11,12 @@ from NaoRobot.modules.helper_funcs.chat_status import (
     user_admin,
     user_admin_no_reply,
 )
-from NaoRobot.modules.helper_funcs.extraction import (
+from EmikoRobot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from NaoRobot.modules.helper_funcs.string_handling import extract_time
-from NaoRobot.modules.log_channel import loggable
+from EmikoRobot.modules.helper_funcs.string_handling import extract_time
+from EmikoRobot.modules.log_channel import loggable
 from telegram import (
     Bot, 
     Chat, 
