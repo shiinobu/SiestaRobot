b5a6d7c52b14ef72d6b3b677d2369a6b451fc81f
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 17:21:16 +0700

    Update and rename NaoRobot/modules/whatanime.py to EmikoRobot/modules/whatanime.py

--- EmikoRobot/modules/whatanime.py
+++ EmikoRobot/modules/whatanime.py
@@ -16,7 +16,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import is_image, is_video
 
-from NaoRobot.events import register as tomori
+from EmikoRobot.events import register as tomori
 
 
 
