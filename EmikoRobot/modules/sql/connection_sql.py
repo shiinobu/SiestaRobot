a7339e8815b65dc315aa14a68a295b3d70977bb3
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:40:44 +0700

    Update and rename NaoRobot/modules/sql/connection_sql.py to EmikoRobot/modules/sql/connection_sql.py

--- EmikoRobot/modules/sql/connection_sql.py
+++ EmikoRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
-from NaoRobot.modules.sql import SESSION, BASE
+from EmikoRobot.modules.sql import SESSION, BASE
 
 
 class ChatAccessConnectionSettings(BASE):
