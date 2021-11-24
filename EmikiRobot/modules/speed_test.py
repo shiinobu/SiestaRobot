e0f6647a1d4feb26ab6ab20d6617172472fa31bf
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:54:32 +0700

    Update and rename NaoRobot/modules/speed_test.py to EmikiRobot/modules/speed_test.py

--- EmikiRobot/modules/speed_test.py
+++ EmikiRobot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from NaoRobot import DEV_USERS, dispatcher
-from NaoRobot.modules.disable import DisableAbleCommandHandler
-from NaoRobot.modules.helper_funcs.chat_status import dev_plus
+from EmikoRobot import DEV_USERS, dispatcher
+from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler
 
