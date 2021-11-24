7af3653ed283a115689ac2ffc2707930f4b53c3a
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 15:10:18 +0700

    Update and rename NaoRobot/modules/cash.py to EmikoRobot/modules/cash.py

--- EmikoRobot/modules/cash.py
+++ EmikoRobot/modules/cash.py
@@ -3,7 +3,7 @@ import requests
 from telegram import Bot, Update
 from telegram.ext import CommandHandler, run_async
 
-from NaoRobot import dispatcher, CASH_API_KEY
+from EmikoRobot import dispatcher, CASH_API_KEY
 
 
 def convert(bot: Bot, update: Update):
