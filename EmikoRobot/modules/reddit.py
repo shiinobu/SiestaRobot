3fd4bc2cffa1be70f6a687814c3c9fa0cb90bfbf
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:43:26 +0700

    Update and rename NaoRobot/modules/reddit.py to EmikoRobot/modules/reddit.py

--- EmikoRobot/modules/reddit.py
+++ EmikoRobot/modules/reddit.py
@@ -19,8 +19,8 @@ SOFTWARE.
 """
 from pyrogram import filters
 
-from NaoRobot import pbot as app, arq
-from NaoRobot.utils.errors import capture_err
+from EmikoRobot import pbot as app, arq
+from EmikoRobot.utils.errors import capture_err
 
 __mod_name__ = "Reddit"
 
