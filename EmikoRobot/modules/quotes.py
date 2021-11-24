45dfd8621cf3c4587acce67c8214bbdb7f06391b
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 16:40:51 +0700

    Update and rename NaoRobot/modules/quotes.py to EmikoRobot/modules/quotes.py

--- EmikoRobot/modules/quotes.py
+++ EmikoRobot/modules/quotes.py
@@ -4,9 +4,9 @@ from traceback import format_exc
 from pyrogram import filters
 from pyrogram.types import Message
 
-from NaoRobot import arq
-from NaoRobot.utils.errors import capture_err
-from NaoRobot import pbot as app
+from EmikoRobot import arq
+from EmikoRobot.utils.errors import capture_err
+from EmikoRobot import pbot as app
 
 
 async def quotify(messages: list):
