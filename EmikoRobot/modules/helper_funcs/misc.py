a2e2f0ce7c204529559f9aac9a7150aeefca5e99
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-24 12:28:30 +0700

    Update and rename NaoRobot/modules/helper_funcs/misc.py to EmikoRobot/modules/helper_funcs/misc.py

--- EmikoRobot/modules/helper_funcs/misc.py
+++ EmikoRobot/modules/helper_funcs/misc.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from NaoRobot import NO_LOAD
+from EmikoRobot import NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
