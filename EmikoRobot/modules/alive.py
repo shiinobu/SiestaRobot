a5b531a32bd806dc48594dfd040c70cc23ead55d
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-25 10:03:50 +0700

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -1,6 +1,6 @@
 import os
 import re
-
+from platform import python_version as kontol
 from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
@@ -22,3 +22,17 @@ async def awake(event):
   TEXT += "**Thanks For Adding Me Here ❤️**"
   BUTTON = [[Button.url("Help", "https://t.me/EmiexRobot?start=help"), Button.url("Support", "https://t.me/emikosupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
+
+
+@register(pattern=("/repo"))
+async def source(event):
+   TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}).** \n\n"
+   TEXT += "**Here is The source of Emiko Robot** \n\n"
+   TEXT += "**Owner repo : [sena-ex](https://t.me/excrybaby)** \n\n"
+   TEXT += f"**Python Version :** `{kontol()}`\n\n"
+   TEXT += f"**Library Version :** `{telever}` \n\n"
+   TEXT += f"**Telethon Version :** `{tlhver}` \n\n"
+   TEXT += f"**Pyrogram Version :** `{pyrover}` \n\n"
+   BUTTON = [[Button.url("Repo", "https://github.com/kennedy-ex/emikorobot"), Button.url("Support", "https://t.me/emikosupport")]]
+   await tbot.send_message(event.chat_id, TEXT,  buttons=BUTTON)
+
