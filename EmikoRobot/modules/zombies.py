e4f55c8c00ff8408eb2a68770f5f2c397e2eae75
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-26 00:48:39 +0700

    Update zombies.py

--- EmikoRobot/modules/zombies.py
+++ EmikoRobot/modules/zombies.py
@@ -37,6 +37,7 @@ UNBAN_RIGHTS = ChatBannedRights(
 OFFICERS = [OWNER_ID] + DEV_USERS + DRAGONS + DEMONS
 
 # Check if user has admin rights
+
 async def is_administrator(user_id: int, message):
     admin = False
     async for user in telethn.iter_participants(
@@ -48,68 +49,53 @@ async def is_administrator(user_id: int, message):
     return admin
 
 
-
 @telethn.on(events.NewMessage(pattern="^[!/]zombies ?(.*)"))
-async def zombies(event):
-    """ For .zombies command, list all the zombies in a chat. """
-
-    con = event.pattern_match.group(1).lower()
+async def rm_deletedacc(show):
+    con = show.pattern_match.group(1).lower()
     del_u = 0
-    del_status = "No Deleted Accounts Found, Group Is Clean."
-
+    del_status = "**Group clean, not found the deleted account.**"
     if con != "clean":
-        find_zombies = await event.respond("Searching For Zombies...")
-        async for user in event.client.iter_participants(event.chat_id):
-
+        await show.reply("`Searching deleted account...`")
+        async for user in show.client.iter_participants(show.chat_id):
             if user.deleted:
                 del_u += 1
                 await sleep(1)
         if del_u > 0:
-            del_status = f"Found **{del_u}** Zombies In This Group.\
-            \nClean Them By Using - `/zombies clean`"
-        await find_zombies.edit(del_status)
-        return
-
-    # Here laying the sanity check
-    chat = await event.get_chat()
+            del_status = (
+                f"**Founding** `{del_u}` **Deleted account/Zombie On this group,"
+                "\nClean it with command** `/zombies clean`"
+            )
+        return await show.edit(del_status)
+    chat = await show.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
-
-    # Well
-    if not await is_administrator(user_id=event.from_id, message=event):
-        await event.respond("You're Not An Admin!")
-        return
-
     if not admin and not creator:
-        await event.respond("I Am Not An Admin Here!")
-        return
-
-    cleaning_zombies = await event.respond("Cleaning Zombies...")
+        return await show.edit("**Sorry you're not admin!**")
+    await show.reply("`Deleting deleted account...`")
     del_u = 0
     del_a = 0
-
-    async for user in event.client.iter_participants(event.chat_id):
+    async for user in telethn.iter_participants(show.chat_id):
         if user.deleted:
             try:
-                await event.client(
-                    EditBannedRequest(event.chat_id, user.id, BANNED_RIGHTS)
+                await show.client(
+                    EditBannedRequest(show.chat_id, user.id, BANNED_RIGHTS)
                 )
             except ChatAdminRequiredError:
-                await cleaning_zombies.edit("I Don't Have Ban Rights In This Group.")
-                return
+                return await show.edit("`Not have a banned rights on this group`")
             except UserAdminInvalidError:
                 del_u -= 1
                 del_a += 1
-            await event.client(EditBannedRequest(event.chat_id, user.id, UNBAN_RIGHTS))
+            await telethn(EditBannedRequest(show.chat_id, user.id, UNBAN_RIGHTS))
             del_u += 1
-
     if del_u > 0:
-        del_status = f"Cleaned `{del_u}` Zombies"
-
+        del_status = f"**Cleaned** `{del_u}` **Zombies**"
     if del_a > 0:
-        del_status = f"Cleaned `{del_u}` Zombies \
-        \n`{del_a}` Zombie Admin Accounts Are Not Removed!"
-
-    await cleaning_zombies.edit(del_status)
+        del_status = (
+            f"**Cleaned** `{del_u}` **Zombies** "
+            f"\n`{del_a}` **Admin zombies not deleted.**"
+        )
+    await show.edit(del_status)
+    await sleep(2)
+    await show.delete()
 
 __mod_name__ = "Zombies"
