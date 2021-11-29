8dda5cb26cabe6d2ae41a33b7b7ec2976f8d25d6
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-11-29 18:18:06 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -2,34 +2,25 @@ __mod_name__ = "Musicplayer"
 
 __help__ = """
 
- ──「 Music stream 」──       
-                    
+ ──「 Music stream 」──                          
 ❂ /play (query or reply audio) to playing music from yourube or your audio telegram.
 ❂ /playlist (choose option) for playing your personal playlist or group playlist.
-
 • Admins only
-
 ❂ /reload - refresh the admincache
 ❂ /pause - for paused the track currently playing.
 ❂ /resume - for resuming the paused track.
 ❂ /skip - skipped to the next track.
 ❂ /end - for stopped the streaming.
 
-
  ──「 Video Stream 」──
-
 ❂ /vplay (query or reply audio) for playing video from YouTube.
-
 • Admins only
-
 ❂ /vpause - paused currently video play.
 ❂ /vresume - resuming the paused video.
 ❂ /vskip - skipping the video.
 ❂ /vstop - stopped video streaming.
 ❂ /vmute - muting assistant on the voice calls.
 ❂ /vunmute - unmuting assistant.
-
-
 ❂ /userbotjoin - for inviting assistant to your groups.
 ❂ /userbotleave - for leaving assistant from yor groups.
 
