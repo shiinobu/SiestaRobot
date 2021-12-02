b08e12566d168cbf26fae55eefbadb4fc26d7695
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-02 19:28:27 +0700

    Update warns_sql.py

--- EmikoRobot/modules/sql/warns_sql.py
+++ EmikoRobot/modules/sql/warns_sql.py
@@ -8,7 +8,7 @@ from sqlalchemy.dialects import postgresql
 class Warns(BASE):
     __tablename__ = "warns"
 
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     chat_id = Column(String(14), primary_key=True)
     num_warns = Column(Integer, default=0)
     reasons = Column(postgresql.ARRAY(UnicodeText))
