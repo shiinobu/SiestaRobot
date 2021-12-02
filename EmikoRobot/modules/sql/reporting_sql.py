2ce45db501901a71f83d715638f8dace4540aec3
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-02 19:27:29 +0700

    Update reporting_sql.py

--- EmikoRobot/modules/sql/reporting_sql.py
+++ EmikoRobot/modules/sql/reporting_sql.py
@@ -7,7 +7,7 @@ from sqlalchemy import Boolean, Column, Integer, String
 
 class ReportingUserSettings(BASE):
     __tablename__ = "user_report_settings"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     should_report = Column(Boolean, default=True)
 
     def __init__(self, user_id):
