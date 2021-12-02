1fd9c34ed1549ea3aab70775c6b8f1f8934bc357
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-02 19:13:45 +0700

    Update users_sql.py

--- EmikoRobot/modules/sql/users_sql.py
+++ EmikoRobot/modules/sql/users_sql.py
@@ -15,7 +15,7 @@ from sqlalchemy import (
 
 class Users(BASE):
     __tablename__ = "users"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     username = Column(UnicodeText)
 
     def __init__(self, user_id, username=None):
