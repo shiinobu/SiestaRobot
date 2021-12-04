7ad8950070e88d778cb829eb56661af485cd1dba
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-04 09:20:27 +0700

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -81,7 +81,7 @@ if ENV:
     API_HASH = os.environ.get("API_HASH", None)
     SESSION_STRING = os.environ.get("SESSION_STRING", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
-    DB_URI = os.environ.get("DATABASE_URL")
+    DB_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     ARQ_API = os.environ.get("ARQ_API", None)
