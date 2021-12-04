2505134257a0468ec35aa304a5c96663a26a1c45
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-04 08:37:37 +0700

    Update userinfo_sql.py

--- EmikoRobot/modules/sql/userinfo_sql.py
+++ EmikoRobot/modules/sql/userinfo_sql.py
@@ -1,7 +1,8 @@
 import threading
 
 from EmikoRobot.modules.sql import BASE, SESSION
-from sqlalchemy import Column, BigInteger, Integer, UnicodeText
+from sqlalchemy import Column, UnicodeText
+from sqlalchemy.sql.sqltypes import BigInteger
 
 
 class UserInfo(BASE):
