8cde482fb3d468461500c0a427475e1da080514a
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-04 10:58:38 +0700

    Update users.py

--- EmikoRobot/modules/users.py
+++ EmikoRobot/modules/users.py
@@ -162,7 +162,7 @@ def __user_info__(user_id):
 
 
 def __stats__():
-    return f"• {sql.num_users()} users, across {sql.num_chats()} chats"
+    return f"× {sql.num_users()} users, across {sql.num_chats()} chats"
 
 
 def __migrate__(old_chat_id, new_chat_id):
