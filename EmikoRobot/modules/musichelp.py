0ef65af7a0f5ffb99aa09bc62445a2bde656f83d
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-09 23:49:51 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,28 +1,19 @@
 __mod_name__ = "Musicplayer"
 
 __help__ = """
+──「 Music stream 」──              
 
- ──「 Music stream 」──                          
 ❂ /play (query or reply audio) to playing music from yourube or your audio telegram.
 ❂ /playlist (choose option) for playing your personal playlist or group playlist.
-• Admins only
-❂ /reload - refresh the admincache
-❂ /pause - for paused the track currently playing.
-❂ /resume - for resuming the paused track.
-❂ /skip - skipped to the next track.
-❂ /end - for stopped the streaming.
 
- ──「 Video Stream 」──
-❂ /vplay (query or reply audio) for playing video from YouTube.
 • Admins only
-❂ /vpause - paused currently video play.
-❂ /vresume - resuming the paused video.
-❂ /vskip - skipping the video.
-❂ /vstop - stopped video streaming.
-❂ /vmute - muting assistant on the voice calls.
-❂ /vunmute - unmuting assistant.
-❂ /userbotjoin - for inviting assistant to your groups.
-❂ /userbotleave - for leaving assistant from yor groups.
+
+❂ /auth - Add to authorized users.
+❂ /unauth - Canceling or deleting from authorized users list.
+❂ /msettings - To open panel settings on musicplayer.
+❂ /setadmin - To set all command only for admins.
+❂ /setmember - Set all command for all members.
+❂ /vol (range 1-200) - To set volume on voice calls.
 
 NOTE : DON'T SPAMMING AT REQUEST SONG OR VIDEO...
 """
