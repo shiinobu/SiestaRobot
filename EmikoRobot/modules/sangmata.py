3a99e15bf180778abf0e3d0ff2df2e767e26f573
kennedy-ex <87056837+kennedy-ex@users.noreply.github.com>
2021-12-12 12:47:06 +0700

    Update sangmata.py

--- EmikoRobot/modules/sangmata.py
+++ EmikoRobot/modules/sangmata.py
@@ -56,3 +56,43 @@ async def lastname(steal):
             )
     except TimeoutError:
         return await puki.edit("`I'm Sick Sorry...`")
+
+
+
+@register(pattern="^/quotly ?(.*)")
+async def quotess(qotli):
+    if qotli.fwd_from:
+        return
+    if not qotli.reply_to_msg_id:
+        return await qotli.edit("```Mohon Balas Ke Pesan```")
+    reply_message = await qotli.get_reply_message()
+    if not reply_message.text:
+        return await qotli.edit("```Mohon Balas Ke Pesan```")
+    chat = "@QuotLyBot"
+    if reply_message.sender.ubot:
+        return await qotli.edit("```Mohon Balas Ke Pesan```")
+    await qotli.edit("```Sedang Memproses Sticker, Mohon Menunggu```")
+    try:
+        async with ubot.conversation(chat) as conv:
+            try:
+                response = conv.wait_event(
+                    events.NewMessage(
+                        incoming=True,
+                        from_users=1031952739))
+                msg = await ubot.forward_messages(chat, reply_message)
+                response = await response
+                """ - don't spam notif - """
+                await ubot.send_read_acknowledge(conv.chat_id)
+            except YouBlockedUserError:
+                return await qotli.reply("```Harap Jangan Blockir @QuotLyBot Buka Blokir Lalu Coba Lagi```")
+            if response.text.startswith("Hi!"):
+                await qotli.edit("```Mohon Menonaktifkan Pengaturan Privasi Forward Anda```")
+            else:
+                await qotli.delete()
+                await ubot.forward_messages(qotli.chat_id, response.message)
+                await ubot.send_read_acknowledge(qotli.chat_id)
+                """ - cleanup chat after completed - """
+                await qotli.client.delete_messages(conv.chat_id,
+                                                   [msg.id, response.id])
+    except TimeoutError:
+        await qotli.edit()
