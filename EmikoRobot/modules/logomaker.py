eff26f4a3d6bb6787dd7b8d687a1a9acf2ee6a03
Firman Aprilian S <55623461+shiinobu@users.noreply.github.com>
2021-12-14 16:23:11 +0700

    Change Username Bot

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
             force_document=False,
         )
         os.remove(flnme)
