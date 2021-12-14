9c8675ed02a438dd2d0e5145d95e1e6fe0b261ff
Firman Aprilian S <55623461+shiinobu@users.noreply.github.com>
2021-12-14 16:31:15 +0700

    Change Name Bot

--- EmikoRobot/modules/google.py
+++ EmikoRobot/modules/google.py
@@ -268,7 +268,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Emiko <==="
+        app_details += "\n\n===> Siesta <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -287,7 +287,7 @@ __help__ = """
 ❂ /github <username>*:* Get information about a GitHub user.
 ❂ /country <country name>*:* Gathering info about given country
 ❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Emiko <query>*:* Emiko answers the query
+❂ Siesta <query>*:* Siesta answers the query
 
-  💡Ex: `Emiko where is Japan?`
+  💡Ex: `Siesta where is Japan?`
 """
