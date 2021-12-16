0d510cd09cfdcf6ce018af500259d1f16c738a29
Firman Aprilian S <55623461+shiinobu@users.noreply.github.com>
2021-12-16 19:00:07 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -5,15 +5,28 @@ __help__ = """
 
 ❂ /play (query or reply audio) to playing music from yourube or your audio telegram.
 ❂ /playlist (choose option) for playing your personal playlist or group playlist.
-
+❂ /song <song name> - download songs you want quickly.
+❂ /search <query> - search videos on youtube with details
+❂ /lyrics <query> - search lyrics from web
+    
 • Admins only
 
+❂ /pause - pause song play.
+❂ /resume - resume song play.
+❂ /skip - play next song.
+❂ /end - stop music play.
 ❂ /auth - Add to authorized users.
 ❂ /unauth - Canceling or deleting from authorized users list.
 ❂ /msettings - To open panel settings on musicplayer.
 ❂ /setadmin - To set all command only for admins.
 ❂ /setmember - Set all command for all members.
 ❂ /vol (range 1-200) - To set volume on voice calls.
+
+• Sudo Users only
+
+❂ /userbotleaveall - remove assistant from all chats.
+❂ /broadcast <reply to message> - globally brodcast replied message to all chats.
+❂ /pmpermit [on/off] - enable/disable pmpermit message.
 
 NOTE : DON'T SPAMMING AT REQUEST SONG OR VIDEO...
 """
