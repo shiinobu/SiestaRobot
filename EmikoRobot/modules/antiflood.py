5c1960f0acad6ff003657a2958dbeb30d566881d
Firman Aprilian S <55623461+shiinobu@users.noreply.github.com>
2021-12-20 01:33:49 +0700

    Added Help Module

--- EmikoRobot/modules/antiflood.py
+++ EmikoRobot/modules/antiflood.py
@@ -397,6 +397,29 @@ def __chat_settings__(chat_id, user_id):
         return "Not enforcing to flood control."
     return "Antiflood has been set to`{}`.".format(limit)
 
+__help__ = """
+──「 Anti-Flood 」──
+
+Antiflood allows you to take action on users that send more than x messages in a row. Exceeding the set flood \
+will result in restricting that user.
+This will mute users if they send more than 10 messages in a row, bots are ignored.
+❂ `/flood`*:* Get the current flood control setting
+
+*Admins only:*
+
+❂ `/setflood <int/'no'/'off'>`*:* enables or disables flood control
+ *Example:* `/setflood 10`
+❂ `/setfloodmode <ban/kick/mute/tban/tmute> <value>`*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+
+*Note:*
+Value must be filled for tban and tmute!!
+It can be:
+⇝ `5m` = 5 minutes
+⇝ `6h` = 6 hours
+⇝ `3d` = 3 days
+⇝ `1w` = 1 week
+ """
+
 __mod_name__ = "Anti-Flood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
