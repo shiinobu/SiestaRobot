da8a5b2e3809240b97474bc97a0b5730266f289a
Firman Aprilian S <55623461+shiinobu@users.noreply.github.com>
2021-12-22 23:05:21 +0700

    Change RM_BG_API

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -96,7 +96,7 @@ if ENV:
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
     DB_URL = os.environ.get("DATABASE_URL")
     DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
-    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
+    REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", "xYCR1ZyK3ZsofjH7Y6hPcyzC")
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     ARQ_API = os.environ.get("ARQ_API", None)
     DONATION_LINK = os.environ.get("DONATION_LINK")
