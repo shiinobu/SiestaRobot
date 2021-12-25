a8172e1b7f90597bed1b5e501b6ef3629226c361
Firman Aprilian S <55623461+shiinobu@users.noreply.github.com>
2021-12-26 00:55:51 +0700

    Change Handlers

--- EmikoRobot/modules/reporting.py
+++ EmikoRobot/modules/reporting.py
@@ -280,7 +280,7 @@ SETTING_HANDLER = CommandHandler("reports", report_setting, run_async=True)
 REPORT_HANDLER = CommandHandler(
     "report", report, filters=Filters.chat_type.groups, run_async=True
 )
-ADMIN_REPORT_HANDLER = RegexHandler(r"(?i)@admin(s)?", report, run_async=True)
+ADMIN_REPORT_HANDLER = MessageHandler(Filters.regex(r"(?i)@admin(s)?"), report)
 
 REPORT_BUTTON_USER_HANDLER = CallbackQueryHandler(
     buttons, pattern=r"report_", run_async=True
