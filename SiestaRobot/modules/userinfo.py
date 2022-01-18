18c257e677e1d17ea6671456d82a544a3326e556
shiinobu <kurogamiaoi1@gmail.com>
2022-01-18 20:24:58 +0700

    Default Fixed

--- SiestaRobot/modules/userinfo.py
+++ SiestaRobot/modules/userinfo.py
@@ -166,13 +166,13 @@ def get_id(update: Update, context: CallbackContext):
 
     elif chat.type == "private":
         msg.reply_text(
-            f"Your id is <code>{chat.id}</code>.", parse_mode=ParseMode.HTML,
+            f"Your id is <code>{chat.id}</code>.",
             parse_mode=ParseMode.HTML,
         )
 
     else:
         msg.reply_text(
-            f"This group's id is <code>{chat.id}</code>.", parse_mode=ParseMode.HTML, 
+            f"This group's id is <code>{chat.id}</code>.",
             parse_mode=ParseMode.HTML,
         )
 
